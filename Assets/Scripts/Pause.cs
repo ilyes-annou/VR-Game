3e57fdace0ad1cd4b000ee958a5409fd11691d08
annoui <90700176+annoui@users.noreply.github.com>
2023-03-17 15:37:10 +0100

    Add files via upload

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public static bool enPause=false;
+	public GameObject menuPause;
+    // Update is called once per frame
+    void Update()
+    {
+
+    	if(Input.GetKeyDown(KeyCode.Return)){
+    		if(enPause){
+    			Reprendre();
+    		}
+    		else {
+    			Pauser();
+    		}
+    	}
+        
+    }
+
+    public void Reprendre(){
+    	menuPause.SetActive(false);
+    	Time.timeScale=1f;
+    	enPause=false;
+
+
+    }
+
+    public void Pauser(){
+    	menuPause.SetActive(true);
+    	Time.timeScale=0f;
+    	enPause=true;
+    }
+
+    public void Carte(){
+    	Time.timeScale=1f;
+    	SceneManager.LoadScene("Map");
+
+    }
+}
+
